01036e83c1fa42ef2ce483256c2cfbecca56f21a
Tatsuhiro Motomatsu <tatsu@Jacob.local>
2024-04-08 02:48:59 +0900

    add: station2クリア

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PracticeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +27,11 @@ Route::get('practice2', function() {
 return response($test);
 });
 
-Route::get('practice3', function() {
-    $test = 'test';
-return response($test);
-});
+// Route::get('practice3', function() {
+//     $test = 'test';
+// return response($test);
+// });
+
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
